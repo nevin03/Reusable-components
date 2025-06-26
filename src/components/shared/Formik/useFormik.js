2bdbb0d52dd4045269569efe26054839c200f9d0
nevin03 <nevinm2021@gmail.com>
2025-06-26 13:52:12 +0530

    updated useFormik with rest

--- src/components/shared/Formik/useFormik.js
+++ src/components/shared/Formik/useFormik.js
@@ -4,11 +4,13 @@ export const useCustomForm = ({
   initialValues,
   validationSchema,
   onSubmit,
+  ...rest
 }) => {
   const formik = useFormik({
     initialValues,
     validationSchema,
     onSubmit,
+    ...rest,
   });
 
   return formik;
