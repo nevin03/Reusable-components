68588abcfda5e4220a1f54484520befdd081a80e
nevin03 <nevinm2021@gmail.com>
2025-06-27 13:01:29 +0530

    moved useAuth hook to hook folder

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,6 @@
+import useAuthStore from "store/useAuthStore";
+
+export const useAuth = () => {
+  const { user, token, isAuthenticated, login, logout } = useAuthStore();
+  return { user, token, isAuthenticated, login, logout };
+};
