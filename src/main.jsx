0de45c46811877f717723187b1cbaec605021b5b
nevin03 <nevinm2021@gmail.com>
2025-06-27 10:01:11 +0530

    configured for zustand

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import useAuthStore from "./store/useAuthStore";
+
+const token = localStorage.getItem("token");
+if (token) {
+  useAuthStore.getState().login({ user: null, token });
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
